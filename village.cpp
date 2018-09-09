0488cc222b657add6121cab6bc85c6bf62d1cd98
Anupam Arya <er.anupamarya@gmail.com>
2018-09-09 12:00:13 +0530

    maximum village profit

---
+++ village.cpp
@@ -0,0 +1,74 @@
+#include<iostream>
+#include<stack>
+#include <algorithm>
+using namespace std;
+
+
+int main()
+{
+	int n;
+	int villages_profit[n];
+	cin>>n;
+	for(int i=0;i<n;i++)
+	{
+		cin>>villages_profit[i];
+	}
+
+
+	bool pm[n][n]={0};
+	for(int i=0;i<n;i++)
+	{
+		for(int j=i+1;j<n;j++)
+		{
+			if(villages_profit[j]%villages_profit[i]==0)
+				pm[i][j]=true;
+		}
+	}
+
+	stack<int> row_stack;
+	stack<int> col_stack;
+	stack<int> profit_stack;
+
+	int max_profit=0;
+	for(int i=0;i<n;i++)
+	{
+//		cout<<"Starating with row "<<i<<endl;
+		int row,col;
+		row=col=i;
+		int profit=villages_profit[i];
+		while(col < n)
+		{
+			if(pm[row][col])
+			{
+	//			cout<<"Push in stack "<<row<<","<<col<<","<<profit<<endl;
+				row_stack.push(row);
+				col_stack.push(col);
+				profit_stack.push(profit);
+				profit+=villages_profit[col];
+				row=col;
+			}
+			col++;
+			if(col == n)
+			{
+	//			cout<<"Path profit is "<<profit<<endl;
+	//			cout<<"XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX"<<endl;
+				max_profit = max (max_profit,profit);
+				if(!row_stack.empty())
+				{
+	//				cout<<"Stack is not empty"<<endl;
+					row=row_stack.top();
+					col=col_stack.top();
+					profit=profit_stack.top();
+					row_stack.pop();
+					col_stack.pop();
+					profit_stack.pop();
+					col++;
+	//				cout<<"pop from stack "<<row<<","<<col<<","<<profit<<endl;
+				}
+			}
+		}
+	//	cout<<"---------------------------------------"<<endl;
+	}
+	cout<<"max profit is "<<max_profit<<endl;
+	return 0;
+}
